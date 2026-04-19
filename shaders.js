a67076d7d01709fed5bbcaad933fe63c56e87ff1
Ph0ebeK1m <phoebekim12006@gmail.com>
2026-04-19 02:00:45 -0500

    fixed refraction ratio

--- shaders.js
+++ shaders.js
@@ -25,7 +25,7 @@ export const causticFragmentShader = `
         vec3 normalTexture = texture2D(uTexture, uv).rgb;
         vec3 normal = normalize(normalTexture);
         vec3 lightDir = normalize(uLight);
-        vec3 ray = refract(lightDir, normal, 1.0/1.25);
+        vec3 ray = refract(lightDir, normal, 1.0/1.33); // uses snell's law  :D air to water use 1.5 for glass
 
         vec3 newPos = vPos.xyz + ray;
         vec3 oldPos = vPos.xyz;
